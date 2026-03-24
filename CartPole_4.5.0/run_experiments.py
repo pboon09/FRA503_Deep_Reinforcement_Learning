4671e2a2200c85b1c24ba7773afc4ec041750b89
pboon09 <pboonchanachai@gmail.com>
2026-03-25 00:26:10 +0800

    config

--- CartPole_4.5.0/run_experiments.py
+++ CartPole_4.5.0/run_experiments.py
@@ -11,7 +11,7 @@ PLOT_SCRIPT = os.path.join(ROOT, "scripts", "Function_based", "visualize", "plot
 
 TASK = "Stabilize-Isaac-Cartpole-v0"
 ALL_ALGOS = ["Linear_Q", "DQN", "MC_REINFORCE", "AC", "PPO"]
-ALGO_ENVS = {"PPO": 256, "Linear_Q": 8, "DQN": 8, "MC_REINFORCE": 8, "AC": 8}
+ALGO_ENVS = {"PPO": 256, "AC": 256, "MC_REINFORCE": 256, "DQN": 32, "Linear_Q": 32}
 
 
 def train(algorithm):
